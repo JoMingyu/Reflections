c58eed1db78c0f548e5f9000d61bb568b59e88e4
PlanB <city7310@naver.com>
2017-10-29 21:15:32 +0900

    [DONE] default_exractor

---
+++ reflections/reflect.py
@@ -0,0 +1,22 @@
+import pkgutil
+
+_resources = []
+
+
+def reflect(package, extractor):
+    _resources.clear()
+    extractor(package)
+
+    return _resources
+
+
+def default_extractor(target):
+    for loader, name, is_package in pkgutil.iter_modules(target.__path__):
+        # Iterate modules
+        if is_package:
+            # If package, append and search deeper
+            _resources.append((loader, name))
+            default_extractor(loader.find_module(name).load_module(name))
+        else:
+            # If module, append
+            _resources.append((loader, name))
