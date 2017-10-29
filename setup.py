d3464e3bf4ba464385c8674d986d44539f48dd6d
PlanB <city7310@naver.com>
2017-10-29 20:41:14 +0900

    [DONE] setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='reflections',
+      version='0.1',
+      url='https://github.com/JoMingyu/Reflections',
+      license='MIT',
+      author='PlanB',
+      author_email='city7310@naver.com',
+      description='Way to list all the modules in a Python package',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=['reflections'])
